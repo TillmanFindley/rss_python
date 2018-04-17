549cf6349171e913bb53b9060c7cf644af7a71e2
Tillman Findley <tillmanfindley@gmail.com>
2018-04-16 23:25:34 -0500

    Add files via upload

--- python_service.py
+++ python_service.py
@@ -1,25 +1,21 @@
 import feedparser
-import pprint
-import re
 import os
+import time
 from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def rss(): 
-    feeds = []
-    RSS_URLS = ['https://twitrss.me/twitter_user_to_rss/user=realDonaldTrump','http://thehill.com/rss/syndicator/19110',
-            'http://thehill.com/taxonomy/term/1132/feed','http://thehill.com/taxonomy/term/1130/feed',
-            'http://thehill.com/taxonomy/term/1131/feed','http://thehill.com/taxonomy/term/1630/feed',
-            'http://thehill.com/taxonomy/term/1778/feed','http://thehill.com/rss/syndicator/19109']
-    for url in RSS_URLS:
-        feeds.append(feedparser.parse(url))
-    for feed in feeds:
-        for post in feed['entries']:
-            print post
-   
-        
-        
-if __name__ == '__main__':
-    app.run(debug=True)
+#-----------------------------------
+#  The following code is invoked when the path portion of the URL matches 
+#         /determineTime/42/determineTime/yyyy
+#   "yyyy" is passed as the value of the input parameter.
+#
+@app.route('/watson/<remainderOfUrl>')
+def determineTime(remainderOfUrl):
+    inp = str(remainderOfUrl)
+    return inp
+    
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
