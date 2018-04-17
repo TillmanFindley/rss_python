6d11972dccaab2b8d2b050559f8d108d7e895301
Tillman Findley <tillmanfindley@gmail.com>
2018-04-17 01:49:51 -0500

    Add files via upload

--- python_service.py
+++ python_service.py
@@ -9,11 +9,11 @@ app = Flask(__name__)
 @app.route('/trump-feed')
 def rss():  
     twitter = 'https://twitrss.me/twitter_user_to_rss/?user=realDonaldTrump'
-    RSS = 'http://thehill.com/rss/syndicator/19110'
     
     res1 = urllib.urlopen(twitter)
     data1 = res1.read()
     print data1
     
+port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(port))
