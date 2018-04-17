b4c5e929a105e73df99848f63bc2a1c0b7f3b7fe
Tillman Findley <tillmanfindley@gmail.com>
2018-04-17 01:10:51 -0500

    Add files via upload

--- python_service.py
+++ python_service.py
@@ -16,6 +16,5 @@ def rss():
     data = res.read()
     return data
         
-port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(port))
+    app.run(debug=True)
