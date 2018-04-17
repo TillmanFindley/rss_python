979f2bc4c81e2b351d6cc965af19ee3e0e646373
Tillman Findley <tillmanfindley@gmail.com>
2018-04-16 23:12:58 -0500

    Add files via upload

--- python_service.py
+++ python_service.py
@@ -6,21 +6,19 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-feeds = []
-RSS_URLS = ['https://twitrss.me/twitter_user_to_rss/user=realDonaldTrump','http://thehill.com/rss/syndicator/19110',
-            'http://thehill.com/taxonomy/term/1132/feed',
-            'http://thehill.com/taxonomy/term/1130/feed','http://thehill.com/taxonomy/term/1131/feed',
-            'http://thehill.com/taxonomy/term/1630/feed',
-            'http://thehill.com/taxonomy/term/1778/feed','http://thehill.com/rss/syndicator/19109']
 @app.route('/')
-def rss():    
+def rss(): 
+    feeds = []
+    RSS_URLS = ['https://twitrss.me/twitter_user_to_rss/user=realDonaldTrump','http://thehill.com/rss/syndicator/19110',
+            'http://thehill.com/taxonomy/term/1132/feed','http://thehill.com/taxonomy/term/1130/feed',
+            'http://thehill.com/taxonomy/term/1131/feed','http://thehill.com/taxonomy/term/1630/feed',
+            'http://thehill.com/taxonomy/term/1778/feed','http://thehill.com/rss/syndicator/19109']
     for url in RSS_URLS:
         feeds.append(feedparser.parse(url))
-
     for feed in feeds:
         for post in feed['entries']:
             print post
-            return post
+   
         
         
 port = os.getenv('PORT', '5000')
