beb877900605b6cbf9215c15c2af1edc63aae027
Tillman Findley <tillmanfindley@gmail.com>
2018-04-17 01:31:52 -0500

    Add files via upload

--- python_service.py
+++ python_service.py
@@ -12,11 +12,8 @@ def rss():
     twitter = 'https://twitrss.me/twitter_user_to_rss/?user=realDonaldTrump'
     RSS = 'http://thehill.com/rss/syndicator/19110'
     
-    res = urllib.urlopen(RSS)
-    data = res.read()
     res1 = urllib.urlopen(twitter)
     data1 = res1.read()
-    print data
     print data1
     
 if __name__ == "__main__":
