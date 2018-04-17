dc1485e4f7196f159ecf074bc3ee4de8796ba890
Tillman Findley <tillmanfindley@gmail.com>
2018-04-16 22:54:30 -0500

    Add files via upload

---
+++ python_service.py
@@ -0,0 +1,15 @@
+import feedparser
+import pprint
+import re
+ 
+feeds = []
+RSS_URLS = ['https://twitrss.me/twitter_user_to_rss/?user=realDonaldTrump','http://thehill.com/rss/syndicator/19110','http://thehill.com/taxonomy/term/1132/feed',
+            'http://thehill.com/taxonomy/term/1130/feed','http://thehill.com/taxonomy/term/1131/feed','http://thehill.com/taxonomy/term/1630/feed',
+            'http://thehill.com/taxonomy/term/1778/feed','http://thehill.com/rss/syndicator/19109']
+    
+for url in RSS_URLS:
+    feeds.append(feedparser.parse(url))
+
+for feed in feeds:
+    for post in feed['entries']:
+        print post
