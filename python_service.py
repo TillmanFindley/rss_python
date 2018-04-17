fc04346d61b89afc739ec252692fd862c11f6074
Tillman Findley <tillmanfindley@gmail.com>
2018-04-16 23:04:14 -0500

    Add files via upload

--- python_service.py
+++ python_service.py
@@ -1,15 +1,28 @@
 import feedparser
 import pprint
 import re
- 
-feeds = []
-RSS_URLS = ['https://twitrss.me/twitter_user_to_rss/?user=realDonaldTrump','http://thehill.com/rss/syndicator/19110','http://thehill.com/taxonomy/term/1132/feed',
-            'http://thehill.com/taxonomy/term/1130/feed','http://thehill.com/taxonomy/term/1131/feed','http://thehill.com/taxonomy/term/1630/feed',
-            'http://thehill.com/taxonomy/term/1778/feed','http://thehill.com/rss/syndicator/19109']
-    
-for url in RSS_URLS:
-    feeds.append(feedparser.parse(url))
+import os
+from flask import Flask, render_template, request, redirect
 
-for feed in feeds:
-    for post in feed['entries']:
-        print post
+app = Flask(__name__)
+
+feeds = []
+RSS_URLS = ['https://twitrss.me/twitter_user_to_rss/user=realDonaldTrump','http://thehill.com/rss/syndicator/19110',
+            'http://thehill.com/taxonomy/term/1132/feed',
+            'http://thehill.com/taxonomy/term/1130/feed','http://thehill.com/taxonomy/term/1131/feed',
+            'http://thehill.com/taxonomy/term/1630/feed',
+            'http://thehill.com/taxonomy/term/1778/feed','http://thehill.com/rss/syndicator/19109']
+@app.route('/')
+def rss():    
+    for url in RSS_URLS:
+        feeds.append(feedparser.parse(url))
+
+    for feed in feeds:
+        for post in feed['entries']:
+            print post
+            return post
+        
+        
+port = os.getenv('PORT', '5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(port))
